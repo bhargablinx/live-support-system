c04fa5fd9807b89a38c6e389b0c94303df727291
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-16 00:07:26 +0530

    feat: implement visitor and conversation creation API service functions (testing)

---
+++ widget/src/lib/api.ts
@@ -0,0 +1,36 @@
+import api from "./axios";
+
+const createVisitor = async (organizationId: string) => {
+    try {
+        const response = await api.post(`/visitor`, {
+            organizationId
+        });
+
+        if (response.data.success) {
+            console.log(response.data);
+            return response.data.data;
+        }
+    } catch (error) {
+        console.log("Error while creating visitor token", error);
+        return null;
+    }
+}
+
+const createConversation = async (organizationId: string, visitorId: string) => {
+    try {
+        const response = await api.post(`/conversation`, {
+            organizationId,
+            visitorId
+        });
+
+        if (response.data.success) {
+            console.log(response.data);
+            return response.data.data;
+        }
+    } catch (error) {
+        console.log("Error while creating conversation", error);
+        return null;
+    }
+}
+
+export { createVisitor, createConversation }
