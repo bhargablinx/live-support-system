4f62a264ebaf8ee395bab87ded9ad41be8342f57
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-16 01:00:24 +0530

    feat: update express app by adding cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,10 @@ import { errorHandler } from "./middleware/errorHandler.js";
 
 const app: Express = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(cookieParser())
