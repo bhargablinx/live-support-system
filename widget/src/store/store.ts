32470d8a9b3a6ad0db8234d8e4c88d2a0a14e699
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-16 01:02:18 +0530

    feat: export type from store

--- widget/src/store/store.ts
+++ widget/src/store/store.ts
@@ -5,4 +5,7 @@ export const store = configureStore({
     reducer: {
         auth: authReducer
     }
-}) 
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
