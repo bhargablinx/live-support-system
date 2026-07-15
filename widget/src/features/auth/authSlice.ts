1cf43f436a1149b69082724f6da1ed7ee7e97a8d
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-15 23:42:54 +0530

    feat: add auth slice

---
+++ widget/src/features/auth/authSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+    organizationId: string | null;
+    visitorToken: string | null;
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: AuthState = {
+    organizationId: null,
+    visitorToken: null,
+    loading: false,
+    error: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setOrganizationId: (state, action) => {
+            state.organizationId = action.payload;
+        },
+        setVisitorToken: (state, action) => {
+            state.visitorToken = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
+    }
+})
+
+export const { setOrganizationId, setVisitorToken, setLoading, setError } = authSlice.actions;
+export default authSlice.reducer
