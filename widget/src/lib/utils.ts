ac7992e1c496b6681148fd0dba2d4edc78d637de
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-16 01:41:49 +0530

    feat: add utilities for saving, getting and removing local data

--- widget/src/lib/utils.ts
+++ widget/src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function saveToLocal(key: string, data) {
+  window.localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function getFromLocal(key: string) {
+  const data = window.localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+}
+
+export function removeFromLocal(key: string) {
+  window.localStorage.removeItem(key);
+}
