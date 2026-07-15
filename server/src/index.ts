505d6aeb35f1b07e9e2b0c78665eb95df94e9312
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-15 15:57:26 +0530

    feat: integrate socket.io into server to enable real-time communication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config"
 import app from "./app.js"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
+    cors: { origin: "*" }
+})
+
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
