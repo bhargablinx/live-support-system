c2e0f3beb3d2138c90b8e660cfa56fc2015c4d75
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-18 23:25:24 +0530

    feat: add socket instance to be accessed by HTTP controllers through express

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,13 @@ const PORT = process.env.PORT
 const httpServer = createServer(app);
 
 export const io = new Server(httpServer, {
-    cors: { origin: "*" }
+    cors: {
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:3000", "http://localhost:3000"],
+        credentials: true
+    }
 })
+
+app.set("io", io);
 
 registerSocketHandlers(io)
 
