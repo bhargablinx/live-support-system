8d6b79d8a73567c3cac49c2250484d6821053a49
bhargablinx <bhargabbhuyan001@gmail.com>
2026-07-27 23:17:25 +0530

    feat: implement file upload functionality for visitors and agents with secure authentication and Cloudinary integration in backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,7 @@ import conversationRouter from './routes/conversation.route.js'
 import agentRouter from './routes/agent.route.js'
 import analyticsRouter from "./routes/analytics.route.js"
 import orgRouter from "./routes/org.route.js"
+import uploadRouter from "./routes/upload.route.js"
 import { errorHandler } from "./middleware/errorHandler.js";
 import { globalLimiter } from "./middleware/rateLimiter.js";
 
@@ -33,6 +34,7 @@ app.use("/api/v1/conversation", conversationRouter)
 app.use("/api/v1/agents", agentRouter)
 app.use("/api/v1/analytics", analyticsRouter)
 app.use("/api/v1/org", orgRouter)
+app.use("/api/v1/upload", uploadRouter)
 
 
 app.use(errorHandler)
--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,11 @@
-import { User } from '../../generated/prisma/index.js';
+import { User, Visitor } from '../../generated/prisma/index.js';
 
 declare global {
     namespace Express {
         interface Request {
             user?: Omit<User, 'passwordHash' | 'refreshToken'>;
+            visitor?: Visitor;
+            uploadType?: 'agent' | 'visitor';
         }
     }
 }
